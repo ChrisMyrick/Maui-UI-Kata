d20591b96ef1e9ecfc3452f337cb0397445ea44c
ChrisM <ChrisM@svmic.com>
2024-05-14 09:27:14 -0500

    Add project files.

---
+++ Maui UI Kata/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui_UI_Kata
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Maui UI Kata/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui_UI_Kata
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
